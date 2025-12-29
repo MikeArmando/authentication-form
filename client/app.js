14e9085381c6252712cf253f08d99273db91c10d
Mike <mike.armando.m@gmail.com>
2025-12-28 16:13:27 -0800

    Added basic javascript form event handle

--- client/app.js
+++ client/app.js
@@ -7,14 +7,17 @@ const linkToSignup = document.getElementById("link-to-signup");
 const signupForm = document.getElementById("signup-form");
 const loginForm = document.getElementById("login-form");
 
-linkToLogin.addEventListener("click", (e) => {
-  e.preventDefault();
+// ------------------------- Event Listeners -------------------------
+linkToLogin.addEventListener("click", (event) => {
+  event.preventDefault();
+
   signupSection.classList.add("hidden");
   loginSection.classList.remove("hidden");
 });
 
-linkToSignup.addEventListener("click", (e) => {
-  e.preventDefault();
+linkToSignup.addEventListener("click", (event) => {
+  event.preventDefault();
+
   loginSection.classList.add("hidden");
   signupSection.classList.remove("hidden");
 });
@@ -30,9 +33,52 @@ if (loginForm) {
 // ------------------------- Logic for Sign Up -------------------------
 function handleSignup(event) {
   event.preventDefault();
+
+  const form = event.target;
+  if (!form.checkValidity()) {
+    showErrors(form);
+    return;
+  }
+
+  const formData = new FormData(form);
+  const userData = Object.fromEntries(formData);
+
+  if (userData.password !== userData["confirm-password"]) {
+    showCustomError(form);
+    return;
+  }
 }
 
 // ------------------------- Logic for Log In -------------------------
 function handleLogin(event) {
   event.preventDefault();
+
+  const form = event.target;
+  if (!form.checkValidity()) {
+    showErrors(form);
+    return;
+  }
+
+  const formData = new FormData(form);
+  const userData = Object.fromEntries(formData);
+}
+
+// ------------------------- Show Errors -------------------------
+function showErrors(form) {
+  Array.from(form.elements).forEach((input) => {
+    if (!input.checkValidity()) {
+      const errorSpan = input.nextElementSibling;
+      if (errorSpan) {
+        errorSpan.textContent = "Invalid";
+        input.classList.add("invalid-input");
+      }
+    }
+  });
+}
+
+// ------------------------------------------------------------
+function showCustomError(form) {
+  const errorSpan = form["confirm-password"].nextElementSibling;
+  errorSpan.textContent = "Passwords do not match!";
+  errorSpan.classList.add("invalid-input");
 }
