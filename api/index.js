7846f9ec95eb80ae4e16db67ac5c3332b4b8d647
Mike <mike.armando.m@gmail.com>
2026-01-23 13:14:17 -0800

    Removed unnecessary code

--- api/index.js
+++ api/index.js
@@ -4,10 +4,8 @@ import cors from "cors";
 import authRoutes from "../server/routes/authRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static("client"));
 app.use(express.json());
 
 app.use("/api/user", authRoutes);
